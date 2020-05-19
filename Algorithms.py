9982bcd3ac74db7ef346326d5528751e3714615c
nahumsa <nahumsaa@gmail.com>
2020-05-19 18:24:12 -0300

    Added QPE

--- Algorithms.py
+++ Algorithms.py
@@ -94,3 +94,38 @@ def inverse_QFT(circuit,n):
     inverseqft = qft_circuit.inverse()
     circuit.append(inverseqft, circuit.qubits[:n])
     return circuit.decompose()
+
+from qiskit.circuit.add_control import add_control
+
+def qpe(circuit, unitary, n_precision,n_ancilla):   
+    """Applies the quantum phase estimation for a given unitary.
+
+    Parameters
+    ------------------------------------------------
+    circuit(qiskit.QuantumCircuit): Quantum Circuit.
+    unitary(np.array): Unitary.
+    n_precision(int): Number of qubits used for precision.
+    n_ancilla(int): Number of qubits used for generating the eigenstates, 
+                    must be len(unitary)//2 = n_ancilla.
+
+    Output
+    ------------------------------------------------
+    circuit(qiskit.QuantumCircuit): Quantum Circuit with
+                                    qft.
+    
+    """
+    assert len(unitary)//2 == n_ancilla, "Ancilla qubits does't match the number needed to expand the eigenstate."    
+    #Ancilla (need to add a way to initialize states)
+    circuit.h([n_precision + ancilla for ancilla in range(n_ancilla)]) 
+    #Precision
+    circuit.h(range(n_precision))
+    #Build unitary
+    U = qsk.extensions.UnitaryGate(unitary, label='U')
+    U_ctrl = add_control(U, num_ctrl_qubits=1, label='Controlled_U',ctrl_state=1)    
+    repetitions = 1
+    for counting_qubit in range(n_precision):
+        for _ in range(repetitions):                        
+            circuit.append(U_ctrl, [counting_qubit,*[n_precision + ancilla for ancilla in range(n_ancilla)]])         
+        repetitions *= 2
+    inverse_QFT(circuit,n_precision)    
+    return qpe
